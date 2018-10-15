53d012925f203e94c65a62e376e39bd4ff3cb57c
Cresponcious <agatica77@gmail.com>
2018-10-14 22:44:32 -0300

    Ejemplo

---
+++ appCurso/src/app/ejmetodo/ejmetodo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ejmetodo',
+  templateUrl: './ejmetodo.component.html',
+  styleUrls: ['./ejmetodo.component.css']
+})
+export class EjmetodoComponent {
+
+  puntuacion = 9;
+  getPuntuacion() {
+  return this.puntuacion;
+  }
+
+}
